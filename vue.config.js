c6d01262d3a2add09a5cbea35fd5fa48e82aaef2
rohmanpendo <rohmanpendo@gmail.com>
2021-10-11 16:07:51 +0700

    refact: pwa manifest icon

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    pwa: {
+        name: 'Glow',
+        themeColor: '#1D4ED8',
+        msTileColor: '#404040',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: '1D4ED8',
+
+        manifestOptions: {
+            name: 'Global Weather',
+        },
+    },
+};
